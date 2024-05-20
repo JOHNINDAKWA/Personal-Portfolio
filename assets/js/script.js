3c43788afc7663a18fea37801154278e6b12d76c
John Indakwa <108904759+JOHNINDAKWA@users.noreply.github.com>
2024-05-21 01:23:03 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,3 +160,59 @@ for (let i = 0; i < navigationLinks.length; i++) {
 }
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  const skills = document.querySelectorAll('.skill-progress-fill');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const fill = entry.target;
+        const percentage = fill.getAttribute('data-percentage');
+        fill.style.setProperty('--target-width', `${percentage}%`);
+        fill.classList.add('active');
+        observer.unobserve(fill); // Optionally stop observing after animation
+      }
+    });
+  }, {
+    threshold: 0.1 // Adjust this value as needed
+  });
+
+  skills.forEach(skill => {
+    observer.observe(skill);
+  });
+});
+
+
+window.onload = function() {
+
+  const hourHand = document.querySelector('.hourHand');
+      const minuteHand = document.querySelector('.minuteHand');
+      const secondHand = document.querySelector('.secondHand');
+      const time = document.querySelector('.time');
+      const clock = document.querySelector('.clock');
+      const audio = document.querySelector('.audio');
+  
+      function setDate(){
+          const today = new Date();
+          
+          const second = today.getSeconds();
+          const secondDeg = ((second / 60) * 360) + 360; 
+          secondHand.style.transform = `rotate(${secondDeg}deg)`;
+        
+          audio.play();
+          
+          const minute = today.getMinutes();
+          const minuteDeg = ((minute / 60) * 360); 
+          minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+  
+          const hour = today.getHours();
+          const hourDeg = ((hour / 10 ) * 360 ); 
+          hourHand.style.transform = `rotate(${hourDeg}deg)`;
+          
+          time.innerHTML = '<span>' + '<strong>' + hour + '</strong>' + ' : ' + minute + ' : ' + '<small>' + second +'</small>'+ '</span>';
+  
+          }
+    
+      setInterval(setDate, 1000);
+   
+  }
